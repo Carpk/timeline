c13cb1cc2cb3d078468c0313341019eb85999f6d
null <zappeln@gmail.com>
2017-08-01 22:16:17 -0500

    dynamically set group2 to europe dataset

---
+++ js/europeData.js
@@ -0,0 +1,43 @@
+var europeDataArray  = [
+    {content: 'WWI',        start: '1914-07-28'},
+    {content: 'WWII',       start: '1939-09-01'},
+    {content: 'Mona Lisa',  start: '1505-06-01'},
+    {content: 'The Scream', start: '1893-06-01'},
+    {content: 'Plato',      start: '-000380-04-25'},
+    {content: 'The Night Watch', start: '1642-06-01'},
+	{content: 'Rome Fell',  start: '0476-09-01', 
+	 title: '<img src="img/RomulusAugustus.jpg" alt=""><br>Romulus Augustulus,<br>overthrown by Germania'},
+	{content: 'Pantheon',   start: '0123-09-01', 
+	 title: '<img src="img/Pantheon.jpg" alt=""><br>Temple in Rome, Italy', description: 'Vis.js comes with a flexible DataSet, which can be'},
+	{content: 'Parthenon', start: '-000439-06-01',
+	 title: '<img src="img/Parthenon.jpg" alt=""><br>Temple in Athens, Greece'},
+	{content: 'Rome founded', start: '-000753-09-01'},
+	{content: 'Crusades',  start: '1200-06-01',
+	 title: '<img src="img/Crusades.jpg" alt="">'},
+	{content: 'Napoleon',   start: '1809-05-18'},
+	{content: 'Stonehedge', start: '-003100-05-18', 
+	 title: '<img src="img/Stonehenge.jpg" alt=""><br>Prehistoric monument in England'},
+	{content: 'Decorative Jug', start: '-001537-05-18', 
+	 title: '<img src="img/Minoan_Decorated_Jug.jpg" alt=""><br>Minoan decorative jug'},
+	{content: 'Knossos',    start: '-001900-05-18',
+	 title: '<img src="img/Knossos.jpg" alt=""><br>Europe\'s oldest city'},
+	{content: 'Alexander the Great', start: '-000336-05-18',
+	 title: '<img src="img/Alexander_the_Great.jpg" alt=""><br>Alexander the Great'},
+	{content: 'Julius Caesar', start: '-000044-03-15',
+	 title: '<img src="img/La_morte_di_Cesare.jpg" alt=""><br>Death of Julius Caesar'},
+	{content: 'Venus Figurines', start: '-026500-03-15',
+	 title: '<img src="img/Venus_of_Willendorf.jpg" alt=""><br>Venus of Willendorf'},
+	{content: 'Barnenez', start: '-004800-03-15',
+	 title: '<img src="img/Barnenez.jpg" alt=""><br>Cairn of Barnenez, located in France'},
+	{content: 'Chauvet Cave', start: '-026500-03-15',
+	 title: '<img src="img/Chauvet.jpg" alt=""><br>Replica of painting in Chauvet Cave'},
+	{content: 'Venus de Brassempouy', start: '-023000-03-15',
+	 title: '<img src="img/Venus_de_Brassempouy.jpg" alt=""><br>Venus de Brassempouy<br>discovered in 1892 France'}
+];
+
+
+
+for (var i=0; i < europeDataArray.length; i++){
+  europeDataArray[i].group=2;
+}
+
