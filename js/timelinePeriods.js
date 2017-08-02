b133b44f339b763b72439136aeaf0baab1b6c28f
null <zappeln@gmail.com>
2017-08-01 21:59:05 -0500

    dynamically assign IDs to TL items

---
+++ js/timelinePeriods.js
@@ -0,0 +1,16 @@
+var periodsDataArray  = [
+	{id: 'KO', group: 2, content: 'Neolithic',      start: '-007000-07-04', end: '-002600-07-04', type: 'background', className: 'col'},
+	{id: 'MI', group: 2, content: 'Minoan',         start: '-002600-07-04', end: '-001100-07-04', type: 'background', className: 'min'},
+    {id: 'AG', group: 2, content: 'ancient Greece', start: '-000800-09-01', end: '-000323-09-01', type: 'background', className: 'greece'},
+    {id: 'HG', group: 2, content: '',               start: '-000323-09-01', end: '0146-06-22', type: 'background', className: 'greece'},
+    {id: 'AR', group: 2, content: 'ancient Rome',   start: '-000510-06-01', end: '0476-06-15', type: 'background', className: 'rome'},
+	{id: 'PA', group: 1, content: '',               start: '-000030-06-01', end: '0641-07-04', type: 'background', className: 'rome'},
+    {id: 'DA', group: 2, content: 'Dark Ages',      start: '0476-06-01',    end: '1400-06-01', type: 'background', className: 'dark'},
+    {id: 'RE', group: 2, content: 'Renaissance',    start: '1300-01-01',    end: '1700-06-01', type: 'background', className: 'renais'},
+    {id: 'AE', group: 1, content: 'ancient Egypt',  start: '-003100-01-01', end: '-000332-06-01', type: 'background', className: 'aegypt'},
+	{id: 'HE', group: 1, content: '',               start: '-000332-06-01', end: '-000030-06-01', type: 'background', className: 'hegypt'},
+    {id: 'ME', group: 3, content: 'Mesopotamia',    start: '-005700-06-01', end: '-000332-06-15', type: 'background', className: 'mesop'},
+    {id: 'US', group: 4, content: 'America',        start: '1776-07-04',    end: '2017-06-15', type: 'background', className: 'usa'},
+	{id: 'CO', group: 4, content: 'Colonies',       start: '1607-07-04',    end: '1776-07-04', type: 'background', className: 'col'},
+	{id: 'MA', group: 4, content: 'Mesoamerica',    start: '-007000-07-04', end: '1492-07-04', type: 'background', className: 'mesoa'}
+];
