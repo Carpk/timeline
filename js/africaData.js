90e69e9f93166ef52e33b18b6a9e2c586fe7cbe4
null <zappeln@gmail.com>
2017-08-04 13:33:26 -0500

    extract Africa data into individual file

---
+++ js/africaData.js
@@ -0,0 +1,13 @@
+var africaDataArray  = [
+	{group: 1, content: 'Cleopatra',  start: '-000030-08-12',
+	 title: '<img src="img/Cleopatra.jpg" alt=""><br>Cleopatra takes her life'},
+	{group: 1, content: 'King Tut',   start: '-001328-05-18',
+	 title: '<img src="img/Tut.jpg" alt=""><br>Tutankhamun\'s death mask'},
+	{group: 1, content: 'Ramesses II', start: '-001246-05-18'},
+	{group: 1, content: 'Narmer',     start: '-003100-05-18',
+	 title: '<img src="img/NarmerPalette.jpg" alt=""><br>The Narmer Palette, depicts the<br>unification of Upper and Lower Egypt'}
+];
+
+for (var i=0; i < africaDataArray.length; i++){
+  africaDataArray[i].group=1;
+}
