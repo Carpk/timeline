2fcf66a69a6fb79e4985e8288c22f6842d6c22e3
null <zappeln@gmail.com>
2017-08-08 10:26:55 -0500

    expand data sets
    data now includes descriptions and larger image tags

--- js/africaData.js
+++ js/africaData.js
@@ -1,13 +1,21 @@
 var africaDataArray  = [
-	{content: 'Cleopatra',  start: '-000030-08-12',
-	 title: '<img src="img/Cleopatra.jpg" alt=""><br>Cleopatra takes her life'},
-	{content: 'King Tut',   start: '-001328-05-18',
-	 title: '<img src="img/Tut.jpg" alt=""><br>Tutankhamun\'s death mask'},
-	{content: 'Ramesses II', start: '-001246-05-18'},
-	{content: 'Narmer',     start: '-003100-05-18',
-	 title: '<img src="img/NarmerPalette.jpg" alt=""><br>The Narmer Palette, depicts the<br>unification of Upper and Lower Egypt'},
-	{content: 'Nok',        start: '-000600-05-18',
-	 title: '<img src="img/Nok_sculpture.jpg" alt=""><br>Nok terracotta<br>Northern Nigeria'}
+  {content: 'Cleopatra',  start: '-000030-08-12',
+   title: '<img src="img/Cleopatra.jpg" alt=""><br>Cleopatra takes her life',
+   image: '<img src="img/Cleopatra.jpg" alt="">',
+   description: ''},
+  {content: 'King Tut',   start: '-001328-05-18',
+   title: '<img src="img/Tut.jpg" alt=""><br>Tutankhamun\'s death mask',
+   image: '<img src="img/Tut.jpg" alt="">',
+   description: ''},
+  {content: 'Ramesses II', start: '-001246-05-18'},
+  {content: 'Narmer',     start: '-003100-05-18',
+   title: '<img src="img/NarmerPalette.jpg" alt=""><br>The Narmer Palette, depicts the<br>unification of Upper and Lower Egypt',
+   image: '<img src="img/NarmerPalette.jpg" alt="">',
+   description: ''},
+  {content: 'Nok',        start: '-000600-05-18',
+   title: '<img src="img/Nok_sculpture.jpg" alt=""><br>Nok terracotta<br>Northern Nigeria',
+   image: '<img src="img/Nok_sculpture.jpg" alt="">',
+   description: ''}
 ];
 
 for (var i=0; i < africaDataArray.length; i++){
