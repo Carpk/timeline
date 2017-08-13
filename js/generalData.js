d31d1068991f6d7ba14440e760bb98c85b40b672
null <zappeln@gmail.com>
2017-08-13 10:50:39 -0500

    empty data for new attributes

--- js/generalData.js
+++ js/generalData.js
@@ -32,10 +32,14 @@ https://en.wikipedia.org/wiki/Art_of_the_Upper_Paleolithic
 https://en.wikipedia.org/wiki/Venus_of_Pet%C5%99kovice
 https://en.wikipedia.org/wiki/Venus_of_Doln%C3%AD_V%C4%9Bstonice
 https://en.wikipedia.org/wiki/Upper_Paleolithic
+https://en.wikipedia.org/wiki/Paleolithic_Europe
 https://en.wikipedia.org/wiki/Magdalenian
 https://en.wikipedia.org/wiki/Mycenaean_Greece
 https://en.wikipedia.org/wiki/Military_of_Mycenaean_Greece
 https://en.wikipedia.org/wiki/Prehistoric_warfare#Paleolithic
 https://en.wikipedia.org/wiki/Lithic_stage
-
+https://en.wikipedia.org/wiki/Neolithic_Revolution
+https://en.wikipedia.org/wiki/Mesoamerica
+https://en.wikipedia.org/wiki/Mesolithic
+https://en.wikipedia.org/wiki/Nimrud_ivories
 **/
